33674e3536e43e617821c0c4588ea1ddd807db96
Marek Narożny <maras.narozny@gmail.com>
2018-02-24 11:24:22 +0100

    capitalized named functionality

--- src/main/java/com/sda/Writer.java
+++ src/main/java/com/sda/Writer.java
@@ -1,11 +1,18 @@
 package com.sda;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class Writer {
     public String sayHello(String name){
+
         String nameInMessage = name;
+        String greeting = "Hello";
+        if(StringUtils.isAllUpperCase(nameInMessage)){
+            greeting = StringUtils.upperCase(greeting);
+        }
         if (nameInMessage==null || "".equals(nameInMessage)){ //porównujemy stałe do wartości w których może wystąpić NullPOinter
             nameInMessage = "my friend";
         }
-        return "Hello, "+ nameInMessage+ "!";
+        return greeting+", "+ nameInMessage+ "!";
     }
 }
--- src/test/java/com/sda/WriterTest.java
+++ src/test/java/com/sda/WriterTest.java
@@ -2,22 +2,29 @@ package com.sda;
 
 import com.sun.xml.internal.ws.policy.AssertionSet;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class WriterTest {
 
-    @Test
-    public void sayHelloWithGivenNameTest() {
-        Writer writer = new Writer();
-        String name = "Szymon";
-        String result = writer.sayHello(name);
+    private Writer writer;
 
-        Assert.assertEquals("Expected hello message is invalid","Hello, Szymon!", result);
-
+    @Before //metoda która zawsze się wykona przed testem
+    public void init(){
+        writer = new Writer();
     }
-    @Test
+
+
+        @Test
+        public void sayHelloWithGivenNameTest() {
+            String name = "Szymon";
+            String result = writer.sayHello(name);
+
+            Assert.assertEquals("Expected hello message is invalid","Hello, Szymon!", result);
+
+        }
+        @Test
     public void sayHelloWithNoNameSpecified(){
-        Writer writer = new Writer();
         String name = "";
 
         String result = writer.sayHello(name);
@@ -28,11 +35,19 @@ public class WriterTest {
 
     @Test
     public void sayHelloWithNullName(){
-        Writer writer = new Writer();
         String name = null;
 
         String result = writer.sayHello(name);
 
         Assert.assertEquals("Hello, my friend!", result);
+    }
+
+    @Test
+    public void sayHelloWithCapitalisedNameSpecified(){
+        String name = "SZYMON";
+
+        String result = writer.sayHello(name);
+
+        Assert.assertEquals("HELLO, SZYMON!", result);
     }
 }
