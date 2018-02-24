c7e7f11dac91aca33ea5bd81737309f8209e8dae
Marek Narożny <maras.narozny@gmail.com>
2018-02-24 12:12:23 +0100

    multiple names functionality

--- src/main/java/com/sda/Writer.java
+++ src/main/java/com/sda/Writer.java
@@ -3,18 +3,33 @@ package com.sda;
 import org.apache.commons.lang3.StringUtils;
 
 public class Writer {
-    public String sayHello(String name){
 
-        String nameInMessage = name;
+    private String convertToMessage(String[] names) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < names.length ; i++) {
+            sb.append(names[i]);
+            if(i<names.length-2){
+                sb.append(", ");
+            }else if(i == names.length-2){
+                sb.append(" and ");
+            }
+        }
+        return sb.toString();
+    }
+
+    public String sayHello(String name) {
+
+        String [] names = name !=null ? name.split(",") : new String[1];
+        String nameSection = names.length==1 ? name : convertToMessage(names);
         String greeting = "Hello";
-        if(StringUtils.isAllUpperCase(nameInMessage)){
+        if (StringUtils.isAllUpperCase(nameSection)) {
             greeting = StringUtils.upperCase(greeting);
 
         }
-      //  if (nameInMessage==null || "".equals(nameInMessage)){ //porównujemy stałe do wartości w których może wystąpić NullPOinter
-        if (StringUtils.isBlank(nameInMessage)){
-            nameInMessage = "my friend";
+        //  if (nameSection==null || "".equals(nameSection)){ //porównujemy stałe do wartości w których może wystąpić NullPOinter
+        if (StringUtils.isBlank(nameSection)) {
+            nameSection = "my friend";
         }
-        return greeting+", "+ nameInMessage+ "!";
+        return greeting + ", " + nameSection + "!";
     }
 }
