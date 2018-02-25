cf8dc88e129c332e1a618d3a4318149646a9a821
Marek Narożny <maras.narozny@gmail.com>
2018-02-25 10:16:02 +0100

    refactor

--- src/test/java/com/sda/bank/BankTest.java
+++ src/test/java/com/sda/bank/BankTest.java
@@ -22,7 +22,7 @@ public class BankTest {
         boolean result = bank.addUser(user);
         //then
         Assert.assertTrue("Method returned false", result);
-        Assert.assertEquals("User is not present in bank",1, bank.getUsers().size());
+        Assert.assertEquals("User is not present in bank",1, bank.getNuberOfUsers());
     }
     @Test
     public void shouldNotAddNullUser(){
@@ -32,6 +32,6 @@ public class BankTest {
         boolean result = bank.addUser(user);
         //then
         Assert.assertFalse("Method returned true", result);
-        Assert.assertEquals("User is present in bank list",0, bank.getUsers().size());
+        Assert.assertEquals("User is present in bank list",0, bank.getNuberOfUsers());
     }
 }
